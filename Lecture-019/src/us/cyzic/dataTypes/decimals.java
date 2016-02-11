2612748642878cd7cf3f200452e63a38a84bc686
Jason C Miller <cyzicusgit@gmail.com>
2016-02-11 12:55:23 -0700

    completes decimals

--- Lecture-019/src/us/cyzic/dataTypes/decimals.java
+++ Lecture-019/src/us/cyzic/dataTypes/decimals.java
@@ -6,13 +6,27 @@ package us.cyzic.dataTypes;
 public class decimals {
     private static int myInt = 5;
     private static float myFloat = 5f;
-    private static double myDouble1 = 5d; // Explicitly tells Java that variable is a double
+    private static double myDouble1 = 5d; // Explicitly tells Java that value is a double
     private static double myDouble2 = 5.4; // But, Java assumes that any decimal entered is a double
 
     public static void main(String[] args) {
+        printDecimals();
+        printDivisions();
+    }
+
+    private static void printDecimals() {
         System.out.println("myInt = " + myInt);
         System.out.println("myFloat = " + myFloat);
         System.out.println("myDouble1 = " + myDouble1);
         System.out.println("myDouble2 = " + myDouble2);
+    }
+
+    private static void printDivisions() {
+        int myInt2 = 5/3;
+        float myFloat2 = myFloat / 3;
+        double myDouble3 = myDouble1 / 3;
+        System.out.println("myInt2 = " + myInt2);
+        System.out.println("myFloat2 = " + myFloat2);
+        System.out.println("myDouble3 = " + myDouble3);
     }
 }
