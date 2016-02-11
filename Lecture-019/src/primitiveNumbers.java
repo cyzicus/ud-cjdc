b1eaaef165be2962193376c003321a593d018568
Jason C Miller <cyzicusgit@gmail.com>
2016-02-11 12:23:25 -0700

    adds class for cataloging primitive numbers

---
+++ Lecture-019/src/primitiveNumbers.java
@@ -0,0 +1,12 @@
+
+public class primitiveNumbers {
+
+    public static void main(String[] args) {
+        // Integers
+        int minInt = -2_147_483_648;
+        int maxInt = 2_147_483_647;
+
+        System.out.println("minInt = " + minInt);
+        System.out.println("maxInt = " + maxInt);
+    }
+}
