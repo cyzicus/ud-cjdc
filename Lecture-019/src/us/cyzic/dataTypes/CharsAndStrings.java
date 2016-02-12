609dd6a94bd85515aaa946bef09ffd5434ebce1a
Jason C Miller <cyzicusgit@gmail.com>
2016-02-12 12:58:44 -0700

    refactors unicode table generator to use row prefixes

--- Lecture-019/src/us/cyzic/dataTypes/CharsAndStrings.java
+++ Lecture-019/src/us/cyzic/dataTypes/CharsAndStrings.java
@@ -8,21 +8,23 @@ public class CharsAndStrings {
     private static char[] columns = {'0', '1', '2', '3', '4', '5',
             '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-    private static String[] rows = {"2F0", "2F1", "2F2", "2F3",
-            "2F4", "2F5", "2F6", "2F7", "2F8", "2F9", "2FA", "2FB"};
+    private static String[] rowPrefixes = {"30", "31"};
 
     public static void main(String[] args) {
         printTable();
     }
 
     private static void printTable() {
-        for(String row : rows) {
-            List<Character> charRow = new ArrayList<>();
-            for(char column: columns) {
-                int hexVal = Integer.parseInt(row + column, 16);
-                charRow.add((char)hexVal);
+
+        for(String rowPrefix : rowPrefixes) {
+            for (char row : columns) {
+                List<Character> charRow = new ArrayList<>();
+                for (char column : columns) {
+                    int hexVal = Integer.parseInt(rowPrefix + row + column, 16);
+                    charRow.add((char) hexVal);
+                }
+                System.out.println(charRow);
             }
-            System.out.println(charRow);
         }
     }
 }
