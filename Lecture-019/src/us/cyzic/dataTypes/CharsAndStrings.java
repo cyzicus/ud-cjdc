2cfa130850e4a8bbf2ec9601e071a597c4982b01
Jason C Miller <cyzicusgit@gmail.com>
2016-02-12 21:28:27 -0700

    completed unicode table

--- Lecture-019/src/us/cyzic/dataTypes/CharsAndStrings.java
+++ Lecture-019/src/us/cyzic/dataTypes/CharsAndStrings.java
@@ -8,7 +8,7 @@ public class CharsAndStrings {
     private static char[] columns = {'0', '1', '2', '3', '4', '5',
             '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-    private static String[] rowPrefixes = {"30", "31"};
+    private static String[] rowPrefixes = {"2E", "2F"};
 
     public static void main(String[] args) {
         printTable();
